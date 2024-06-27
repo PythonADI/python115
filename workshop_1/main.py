9df399866eb53af733ba0c09302d104b8f14653b
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-06-27 19:29:06 +0400

    Add workshop 1 code

---
+++ workshop_1/main.py
@@ -0,0 +1,24 @@
+# this is a comment
+a = 5  # assignment operator
+name: str = "John"
+
+print("Hello, World!")
+print(name)
+print(a)
+
+a: int = 21
+b = 20
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** 2)
+print(a // b)
+
+print(type("5"), type(5), type(5.), type(True))
+
+# concatenation
+greeting: str = "Hello" + " " + name
+print(greeting)
